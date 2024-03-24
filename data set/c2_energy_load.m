db92e992a77028f8512a180062f5a94fe63f2c24
hdavin <164771158+hdavin@users.noreply.github.com>
2024-03-25 00:44:14 +0800

    Add files via upload

---
+++ data set/c2_energy_load.m
@@ -0,0 +1,31 @@
+clear;clc;close all
+%% Calculate the energy load of prosumers.
+close all
+Times=[1 24];
+Users=1:100; % 100 prosumers
+Energy_load_data=[];% Initialize the energy load of prosumer.
+for j=Users
+    disp(['Progress: ',num2str(j/length(Users)*100),'%'])
+    cd 'prosumer energy load'\
+    cd(strcat('user',num2str(j)));
+    load(strcat('user',num2str(j),'.mat'));
+    for i=Times
+        for k1=1:length(userData)
+            temp1(k1,1)=str2double(userData{k1,5});
+            temp1(k1,2)=str2double(userData{k1,7});
+        end
+        for k2=1:48
+            index1=find(temp1(:,1)==k2);
+            temp2(1:length(index1),k2)=temp1(index1,2);
+        end
+        consumption=zeros(size(temp2,1),24);
+        for k3=1:24
+            consumption(:,k3)=temp2(:,2*(k3-1)+1)+temp2(:,2*k3);%Merge the half-hour energy load records into hourly records
+        end
+    end
+    temp3=max(consumption);
+    Index=find(temp3<1500 & temp3>0);
+    Energy_load_data=[Energy_load_data;consumption(Index,:)];
+    cd ..;cd ..
+end
+save('Energy_load_data.mat','Energy_load_data');
